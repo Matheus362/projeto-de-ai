b50132aa81863dfaf782e2117554e521b0cd41e4
Claud <Claudioh.l.melo2003@gmail.com>
2025-11-19 19:46:09 -0300

    Sistema de ratings atualizado

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,16 +3,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.ML;
 using Microsoft.ML;
 using ML_2025.Models;
+using System.IO;
 
 public class IndexModel : PageModel
 {
- 
+    private readonly PredictionEngine<Produto, ProductPrediction> _predictionEngine;
 
-    private readonly PredictionEngine<Produto, ProductPrediction> _predictionEngine; 
     public IndexModel(PredictionEngine<Produto, ProductPrediction> predictionEngine)
-        {
+    {
         _predictionEngine = predictionEngine;
     }
+
     public List<Produto> Resultados { get; set; }
 
     [BindProperty]
@@ -28,4 +29,34 @@ public class IndexModel : PageModel
             PredictionResult = _predictionEngine.Predict(input);
         }
     }
+
+    public IActionResult OnPostRating([FromBody] RatingInput dados)
+    {
+        string pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "ratings");
+
+        string nomeArquivo = $"ratings_.csv";
+
+        string csv =
+            $"ratings_.csv\n" +
+             $"{dados.Input};{dados.Positivo}\r\n";
+
+        string caminho = Path.Combine(pasta, nomeArquivo);
+
+        if (!System.IO.File.Exists(caminho))
+        {
+            string cabecalho = "Input;Positivo" + Environment.NewLine;
+            System.IO.File.WriteAllText(caminho, cabecalho);
+        }
+        string novaLinha = $"{dados.Input};{dados.Positivo}{Environment.NewLine}";
+        System.IO.File.AppendAllText(caminho, novaLinha);
+
+        return new JsonResult(new { sucesso = true, mensagem = "Obrigado pelo feedback!" });
+    }
+    public class RatingInput
+    {
+        public string Input { get; set; }
+        public bool Positivo { get; set; }
+    }
+
+
 }
